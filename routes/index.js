a98f0c2eceab741642522f46d77c75ec94aa72d5
steinsphang <824780925@qq.com>
2015-11-11 23:49:36 +0800

    添加了文件上传模块

--- routes/index.js
+++ routes/index.js
@@ -1,52 +1,83 @@
 var express = require('express');
 var router = express.Router();
 var nodemailer = require("nodemailer");
+var multiparty = require('multiparty');
+var util = require('util');
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var
-      Title=req.query.Title,
-      Name=req.query.Name,
-      Email=req.query.Email,
-      Content=req.query.Content;
-  console.log(Title);
-  console.log(Name);
-  console.log(Email);
-  console.log(Content);
   res.render('index', { title: 'Express' });
 
 });
 
 //处理post表单请求
 router.post('/', function(req, res, next) {
-  var
-      Title=req.body.Title,
-      Name=req.body.Name,
-      Email=req.body.Email,
-      Content=req.body.Content;
-  console.log(Title);
-  console.log(Name);
-  console.log(Email);
-  console.log(Content);
+  var form = new multiparty.Form({uploadDir: './public/files/'});
+  form.parse(req, function(err, fields, files) {
+    var fieldsTmp = JSON.stringify(fields, null, 2);
+    var filesTmp = JSON.stringify(files, null, 2);
+    if (err) {
+      console.log('parse error: ' + err);
+    } else {
+      //console.log(fieldsTmp);
+      var
+          Title=fields.Title[0],
+          Name=fields.Name[0],
+          Email=fields.Email[0],
+          Content=fields.Content[0];
+      console.log(Title);
+      console.log(Name);
+      console.log(Email);
+      console.log(Content);
 
-  var user="autoapply@ewaybot.com",pass="Ewaybot01";
-  var smtpTransport = nodemailer.createTransport({
-    service:"QQ",
-    auth:{
-      user:user,
-      pass:pass
+
+
+
+      //console.log('parse files: ' + filesTmp);
+      var inputFile = files.Upload[0];
+      //console.log(inputFile);
+      var uploadedPath = inputFile.path;
+      var dstPath = './public/files/' + inputFile.originalFilename;
+      //重命名为真实文件名
+      fs.rename(uploadedPath, dstPath, function (err) {
+        if (err) {
+          console.log('rename error: ' + err);
+        } else {
+          console.log('rename ok');
+        }
+      });
+
+
+//发送邮件模块
+      var user="autoapply@ewaybot.com",pass="Ewaybot01";
+      var smtpTransport = nodemailer.createTransport({
+        service:"QQ",
+        auth:{
+          user:user,
+          pass:pass
+        }
+      });
+      smtpTransport.sendMail({
+        from:"autoapply<"+user+">",
+        to:"<pengchongfu@126.com>",
+        subject:Title,
+        html:'<b>姓名：</b>'+Name+'<br/>'+'<b>邮箱：</b>'+Email+'<br/>'+'<b>内容：</b>'+Content+'<br/>',
+        attachments:[{
+          filename:inputFile.originalFilename,
+          path:dstPath
+        }]
+      },function(err,res){
+        if(err){
+          return console.log(err,res);
+        }
+      });
+
+
     }
   });
-  smtpTransport.sendMail({
-    from:"彭崇甫<"+user+">",
-    to:"<pengchongfu@126.com>",
-    subject:Title,
-    html:'<b>姓名：</b>'+Name+'<br/>'+'<b>邮箱：</b>'+Email+'<br/>'+'<b>内容：</b>'+Content+'<br/>'
-  },function(err,res){
-    if(err){
-      return console.log(err,res);
-    }
-  });
+
+
   res.render('index', { title: 'Express' });
 
 });
