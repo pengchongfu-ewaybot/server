05bb853664f576dee1ff9f859ecf360b3aa1337c
steinsphang <824780925@qq.com>
2015-11-13 14:18:26 +0800

    "修改小错误"

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/users', users);
+//app.use('/users', users);
 app.use('/thanks', thanks);
 app.use('/', routes);
 
